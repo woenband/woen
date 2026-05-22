b20d54d8f6dc236b62110537dcaa79946f20a0cc
woenband <mhjlam@gmail.com>
2026-05-23 01:28:51 +0200

    Update URLs now that site root is at /

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import './App.css';
 function App() {
   return (
     <EasterEggProvider>
-      <Router basename="/woen/">
+      <Router>
         <div className="App">
           <Navbar />
           <Routes>
